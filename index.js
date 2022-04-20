7c7251a3e6f46019aad97a6be80c7ecfa486e81b
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-04-20 01:20:49 -0300

    requisitos bonus ok

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ app.get('/is-today-holiday', (req, res) => {
     } else {
         res.send(`Sim, hoje é ${holidayValidation[0].name}`);
     }
+});
+
+app.get('/holidays/:monthNumber', (req, res) => {
+    const mes = req.params.monthNumber;
+
+    const monthlyHoliday = holidays.filter(holiday => {
+        const holidayMonth = holiday.date.split('/');
+        if (holidayMonth[0] === mes) return true;
+        else return false;
+    })
+
+    res.send(monthlyHoliday.length === 0 ? 'Não há feriados neste mês' : monthlyHoliday);
 })
 
 app.listen(4000, () => {
